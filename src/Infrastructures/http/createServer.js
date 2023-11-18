6e9fe3fcd2e018c2d13dbe36d450a7f81169750e
Tito Candra Septio <titocandraseptio@172-10-48-40.lightspeed.okcbok.sbcglobal.net>
2023-11-18 15:10:35 +0700

    fixing hello world

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -54,7 +54,13 @@ const createServer = async (container) => {
     }
   ]);
 
-  
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      value: 'Hello World!',
+    }),
+  });
 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
